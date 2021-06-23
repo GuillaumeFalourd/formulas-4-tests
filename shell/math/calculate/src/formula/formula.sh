1e6b6fb0747d822850d16a11c29f5c4b3294876c
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-06-23 17:22:24 -0300

    update rit shell math calculate formula
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- shell/math/calculate/src/formula/formula.sh
+++ shell/math/calculate/src/formula/formula.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 runFormula() {
-  if [ "sum" = "$OPERATION" ]; then
+  if [ "sum" = "$RIT_OPERATION" ]; then
     echo "rit shell math sum numbers --rit_number_one=$RIT_NUMBER_ONE --rit_number_two=$RIT_NUMBER_TWO"
-  elif [ "multiplication" = "$OPERATION" ]; then
+  elif [ "multiplication" = "$RIT_OPERATION" ]; then
     echo "rit shell math multiply numbers --rit_number_one=$RIT_NUMBER_ONE --rit_number_two=$RIT_NUMBER_TWO"
   else
-    echo "Unexpected operation type: $OPERATION"
+    echo "Unexpected operation type: $RIT_OPERATION"
   fi
 }
